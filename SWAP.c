530ad73cb71ccb8a9c8b82622c5d99295309b127
Lijo Jolly <lijojolly10@gmail.com>
2024-03-22 00:19:32 +0530

    Add files via upload

---
+++ SWAP.c
@@ -0,0 +1,14 @@
+//To swap numbers of A and B
+#include <stdio.h>
+void main()
+{
+    int a,b,temp;
+    a=10;
+    b=15;
+    temp=a;
+    a=b;
+    b=temp;
+    
+    printf("\nYour swapped Number of a is %d",a);
+    printf("\nYour swapper Number of b is %d",b);
+}
